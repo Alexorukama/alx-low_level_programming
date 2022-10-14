53f04c5e92da63afb750c402d29c8a9ceda32e19
Alexorukamaclear <Alextjani@gmail.com>
2022-10-13 21:59:50 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always Return 0 (Success)
+*/
+
+int main(void)
+{
+	/* prints to stout using printf */
+	printf("with proper gramar, but the outcome is a piece of art,\n");
+	return (0)
+}
