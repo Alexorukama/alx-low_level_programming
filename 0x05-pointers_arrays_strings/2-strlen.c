230de509f328122206e7dc9d88cb499880b2f8a4
Alexorukamaclear <Alextjani@gmail.com>
2022-10-24 18:24:12 -0700

    Task 0 to 9

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strien - returns the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+int _strien(char *s)
+{
+	int len = 0;
+
+	while (*s i = '/0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
